64db725004f4a612a76872db7023f62e4a4c80a6
Emma Stone <emmston@email.unc.edu>
2022-08-26 14:05:17 -0400

    completed ex01_chardle

---
+++ exercises/ex01_chardle.py
@@ -0,0 +1,39 @@
+"""EX01 - Chardle - A cute step toward Wordle."""
+__author__ = "730544721"
+
+
+first_word:str = input("Enter a 5-character word: ")
+if len(first_word) != 5:
+    print("Error: Word must contain 5 characters")
+    exit()
+character:str = input("Enter a single character: ")
+if len(character) != 1:
+    print("Error: Character must be a single character")
+    exit()
+count: int = 0
+
+
+print("Searching for " + character + " in " + first_word)
+
+if character == first_word[0]:
+    print(character + " is found at index 0 ")
+    count += 1 
+if character == first_word[1]: 
+    print(character + " is found at index 1 ")
+    count += 1
+if character == first_word[2]:
+    print(character + " is found at index 2 ")
+    count += 1
+if character == first_word[3]:
+    print(character + " is found at index 3 ")
+    count += 1
+if character == first_word[4]: 
+    print(character + " is found at index 4 ")
+    count += 1
+
+if count == 1:
+    print(1 + " instance of " + character + " in " + first_word)
+if count > 1:
+    print(f"{count} instances of {character} in {first_word}")
+if character == 0:
+    print("No instances of " + character + " in " + first_word)
